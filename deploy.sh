664a30db187b6e04453b3489d020b21a4b69f27b
maheshveera <mahesh.veerabathiran@capitalone.com>
2017-06-15 23:26:35 -0400

    commiting local changes

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,18 @@
 
-sudo apt-get update
+sudo apt-get --assume-yes update
+
+# Install git
+sudo apt-get install git
+
+# Git clone setup
+git clone https://github.com/maheshveera/dockersetup.git -b development
 
 sudo apt-get install \
     linux-image-extra-$(uname -r) \
     linux-image-extra-virtual
 
 # Install packages to allow apt to use a repository over HTTPS
-sudo apt-get install \
+sudo apt-get --assume-yes install \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -15,11 +21,16 @@ sudo apt-get install \
 # Add Docker’s official GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
 # Update the apt package index.
-sudo apt-get update
+sudo apt-get --assume-yes update
 
 # Use this command to install the latest version of Docker
-sudo apt-get install docker-ce
+sudo apt-get --assume-yes install docker-ce
 
 # Post installation scripts
 # Create the docker group
@@ -27,3 +38,27 @@ sudo groupadd docker
 
 # Add your user to the docker group
 sudo usermod -aG docker $USER
+
+
+cd dockersetup
+
+# Deploying elasticsearch & kibana
+docker stack deploy -c monitoring-components-stack.yml monitors
+
+# Deploying swarm log aggregators
+docker stack deploy -c swarm-monitor-stack.yml swarmx
+
+# Preparing kafka container deployment
+docker build -t kafkadocker_kafka .
+#export HOST_IP_ADDRESS=54.86.54.84 # Public IP address
+export HOST_IP_ADDRESS=$(curl "http://169.254.169.254/latest/meta-data/public-ipv4")
+
+# Pull customized logstash-jmx image
+# Optional
+# docker pull dironman/logstash-jmx
+
+# Set label for kafka node
+docker node update --label-add SwarmNodeName=kafka ksgwy2cela7n88kfwxudzp68q # replace with kafka swarm node Id
+
+# Get public ip address
+# curl "http://169.254.169.254/latest/meta-data/public-ipv4"
